866ca9c3f0d6a39937981f3226970cfe7209be22
evangelinejohanna <evajo812@gmail.com>
2024-09-04 14:49:58 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ function App() {
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+          React essentials include components, props and states.
         </p>
       </header>
       <main>
